887acd360eee65d072c7422eedafbde707fdf511
RoXoM <xyy94813@sina.com>
2023-07-17 21:06:11 +0800

    feat: 补充 HawkEye 相关类型

--- index.d.ts
+++ index.d.ts
@@ -989,6 +989,43 @@ declare global {
 
 
     /**
+     * 
+     * 鹰眼的初始化参数
+     * 
+     * @public
+     */
+    export interface HawkEyeOptions {
+      autoMove?: boolean; // 是否随主图视口变化移动
+      showRectangle?: boolean; // 是否显示视口矩形
+      showButton?: boolean; // 是否显示打开关闭的按钮
+      isOpen?: boolean; // 默认是否展开
+      mapStyle?: string; // 缩略图要显示的地图自定义样式，如'amap://styles/dark'
+      layers?: any[]; // 缩略图要显示的图层类型，默认为普通矢量地图
+      width?: string; // 缩略图的宽度，同CSS，如'200px'
+      height?: string; // 缩略图的高度，同CSS，如'200px'
+      offset?: [number, number]; // 缩略图距离地图右下角的像素距离，如 [2,2]
+      borderStyle?: string; // 缩略图的边框样式，同CSS，如"double solid solid double"
+      borderColor?: string; // 缩略图的高度，同CSS，如'silver'
+      borderRadius?: string; // 缩略图的高度，同CSS，如'5px'
+      borderWidth?: string; // 缩略图的高度，同CSS，如'2px'
+      buttonSize?: string; // 箭头按钮的像素尺寸，同CSS，如'12px'
+    }
+
+    /**
+     * 鹰眼控件，用于显示缩略地图，显示于地图右下角，可以随主图的视口变化而变化，也可以配置成固定位置实现类似于南海附图的效果
+     * https://lbs.amap.com/api/javascript-api-v2/documentation#hawkeye
+     * 
+     * @public
+     * @param {HawkEyeOptions} options
+     * @class HawkEye
+     * @extends {Control}
+     */
+    export class HawkEye extends AMap.Control {
+      constructor(options?: HawkEyeOptions);
+    }
+
+
+    /**
      * 图层信息，需要包含图层对象
      * 
      * @public
